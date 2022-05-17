3c15cb310efddaeb9d97e70f41ee1290caf44ce3
svladiko <120291Vv>
2022-05-17 13:27:05 +0300

    Added one more test route

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const port = process.env.PORT || 3000
 app.get('/', (req, res) => {
     res.send('Hello bro')
 })
+app.get('/v', (req, res) => {
+    res.send('First step done')
+})
 
 app.listen(port, () => {
     console.log(`App is run on ${port}`)
